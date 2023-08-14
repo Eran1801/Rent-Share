27c6321515833b7f333c1762ee4b8fe9beef0d36
Eran <Eranlevy9@gmail.com>
2023-08-14 11:17:38 +0300

    minor changes in views.py

--- Users/views.py
+++ Users/views.py
@@ -1,44 +1,3 @@
-# from django.http.response import JsonResponse
-# from django.shortcuts import render
-# from django.views.decorators.csrf import \
-#     csrf_exempt  # will be used to exempt the CSRF token (Angular will handle CSRF token)
-# from rest_framework.parsers import JSONParser
-
-# from Users.models import Users
-# from Users.serializers import UsersSerializer
-
-# '''
-# By adding @csrf_exempt to the view function, you're telling Django that
-# this view doesn't require CSRF protection.
-# This allows your Angular frontend to make POST, PUT, and DELETE requests
-# to the API without including CSRF tokens in those requests.
-# '''
-# @csrf_exempt
-# def users_api(request, user_id=0):
-#     if request.method == 'GET':
-#         users = Users.objects.all()
-#         users_serializer = UsersSerializer(users, many=True)
-#         return JsonResponse(users_serializer.data, safe=False)
-#     elif request.method == 'POST':
-#         users_data = JSONParser().parse(request)
-#         users_serializer = UsersSerializer(data=users_data)
-#         if users_serializer.is_valid():
-#             users_serializer.save()
-#             return JsonResponse("Added Successfully!!", safe=False)
-#         return JsonResponse("Failed to Add.", safe=False)
-    # elif request.method == 'PUT':
-    #     users_data = JSONParser().parse(request)
-    #     users = Users.objects.get(user_id=users_data['user_id'])
-    #     users_serializer = UsersSerializer(users, data=users_data)
-    #     if users_serializer.is_valid():
-    #         users_serializer.save()
-    #         return JsonResponse("Updated Successfully!!", safe=False)
-    #     return JsonResponse("Failed to Update.", safe=False)
-#     elif request.method == 'DELETsE':
-#         users = Users.objects.get(user_id=user_id)
-#         users.delete()
-#         return JsonResponse("Deleted Successfully!!", safe=False)
-
 from django.db import connection
 from django.http.response import JsonResponse
 from django.shortcuts import redirect
@@ -53,41 +12,57 @@ import hashlib
 import logging
 from validate_email import validate_email
 
+# Define the logger at the module level
+logger = logging.getLogger(__name__)
 
 def check_valid_password(pas:str) -> bool:
-    pattern = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)' # contains at least one upper in the begging and lower letter and number
+    pattern = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)' # contains at least one upper and one lower letter and number.
     return True if re.match(pattern,pas) and len(pas) >= 8 else False # adding the big&equal from 8
 
-def hash_password(plain_password:str):
+def hash_password(plain_password:str) -> str:
     sha256 = hashlib.sha256()
     sha256.update(plain_password.encode('utf-8'))
     hashed_password = sha256.hexdigest()
 
     return hashed_password
 
+def email_exists(email:str) -> bool:
+    '''exists() method returns True if user_email already in the db'''
+    return Users.objects.filter(user_email=email).exists()
+
+def phone_exists(phone:str)-> bool:
+    return Users.objects.filter(user_phone=phone).exists()
+
+
+#! HOW TO REMOVE ALL THE REGISTERS USERS IN THE DATA BASE ?!! 
+
 @csrf_exempt
 def register(request, user_id = 0): 
 
     logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger(__name__)  # Get logger instance
 
     if request.method == 'POST':
         user_data = JSONParser().parse(request) # access individual fields, users_data.get('field_name')
         
         user_full_name = user_data.get('user_full_name')
-        user_email = user_data.get('user_email')
+        user_email = user_data.get('user_email').lower() # lower case email
         user_phone_number = user_data.get('user_phone')
         user_password = user_data.get('user_password')
         user_password_2 = user_data.get('user_password_2')
         
         # checks valid register input from user
-        check_full_name: bool = True if len(user_full_name) > 4 else False
+        check_full_name: bool = True if len(user_full_name) >= 4 and user_full_name.count(' ') > 0 else False
         check_email = validate_email(user_email)
         check_phone_number = True if len(user_phone_number) >= 10 else False
         check_password = check_valid_password(user_password)
 
-        if user_password == user_password_2: # checking if 2 user passwords is equal
-            user_data['user_password'] = hash_password(user_data['user_password']) # encrypt before saving
+        if user_password == user_password_2: # checking if 2 user passwords are equal
+            if email_exists(user_email):
+                return JsonResponse('Email already exists',safe=False)
+            if phone_exists(user_phone_number):
+                return JsonResponse('Phone number already exists',safe=False)
+            
+            user_data['user_password'] = hash_password(user_password) # encrypt before saving
             if check_full_name and check_phone_number and check_password and check_email: # check if all true
                     del user_data['user_password_2']
                     users_serializer = UsersSerializer(data=user_data)
@@ -97,6 +72,8 @@ def register(request, user_id = 0):
                     else:
                         logger.debug(users_serializer.errors)
                         return JsonResponse("Register Fails", safe=False)
+            else:
+                return JsonResponse('Something wrong with name|phone|password|email',safe=False)
         else:
             return JsonResponse("Passwords don't match.", safe=False)
         
@@ -111,29 +88,19 @@ def login(request):
         
         user_data = JSONParser().parse(request)
 
-        login_email_address = user_data.get('login_email')
-        login_password = user_data.get('login_password')
+        login_email_address = user_data.get('user_email').lower() # lower case email 
+        login_password = user_data.get('user_password')
 
         # encrypt user password for check similarity in the db
         hash_password_login = hash_password(login_password) 
-
-        # with connection.cursor() as cursor:
-        #     query = "SELECT user_password FROM Users_users WHERE user_email = %s and password = %s"
-        #     cursor.execute(query, (login_email_address,hash_password_login))
-        #     user_record = cursor.fetchone()
-        #     if hash_password_login == user_record[1]: # hash password from db
-        #         redirect('/home') 
-            
         
-        # more secure option / needs to exam:
         try:
             user = Users.objects.get(user_email=login_email_address) # retrieve user from db based on email
             if user.user_password == hash_password_login:
-                return JsonResponse("Passwords match.", safe=False)
+                return JsonResponse("Passwords match. Login successfully", safe=False)
             else:
-                return JsonResponse("Passwords don't match.", safe=False)
+                return JsonResponse("Passwords don't match. Login fail", safe=False)
         except Users.DoesNotExist:
             return JsonResponse("User not found.", safe=False)
         
-        # TODO : add a function that gets the email of the user and checks if it's unique.
-        # TODO : checks if it's already in the db 
+      
