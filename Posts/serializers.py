0c50360a5ba99fb4ae4f3e2f9a916d89badbf858
Eran <Eranlevy9@gmail.com>
2023-08-15 12:37:34 +0300

    add serializer for Image process

--- Posts/serializers.py
+++ Posts/serializers.py
@@ -4,9 +4,14 @@ from Posts.models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
+
+        proof_image_confirmed = serializers.ImageField(max_length=None, use_url=False)
+        driving_license = serializers.ImageField(max_length=None, use_url=False)
+        apartment_pic_1 = serializers.ImageField(max_length=None, use_url=False)  # Handle optional image file
+
         fields = ('post_id','post_user','post_city','post_street',
                  'post_apartment_number','post_apartment_price',
                  'post_rent_start','post_rent_end',
-                 'proof_image','driving_license','post_description','proof_image_confirmed',
-                 'apartment_pic_1')
+                 'proof_image',driving_license,'post_description',proof_image_confirmed,
+                 apartment_pic_1)
         # ,'apartment_pic_2','apartment_pic_3','apartment_pic_4'
