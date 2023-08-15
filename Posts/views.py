d06e69de7a1a8207aaf331dfdc4d4e9013668913
Eran <Eranlevy9@gmail.com>
2023-08-15 12:18:40 +0300

    change again

--- Posts/views.py
+++ Posts/views.py
@@ -19,14 +19,14 @@ def add_post(request):
     if request.method == 'POST':
         post_data = JSONParser().parse(request)
 
-        post_user_email = post_data.get('user_email')
-        user = Users.objects.get(user_email=post_user_email)
-    
-        # # Fetch the Users object based on the email
-        # try:
-        #     user = Users.objects.get(user_email=post_user_email)
-        # except Users.DoesNotExist:
-        #     return HttpResponseServerError('User not found')
+        # post_user_email = post_data.get('user_email')
+        post_user_email = post_data.get('user', {}).get('user_email')
+
+        # Fetch the Users object based on the email
+        try:
+            user = Users.objects.get(user_email=post_user_email)
+        except Users.DoesNotExist:
+            return HttpResponseServerError('User not found')
         
         logger.info("User found.")
 
@@ -49,7 +49,7 @@ def add_post(request):
         post_description = post_data.get('post_description')
 
         post_data_dict = {
-            'post_user': user.id,
+            'post_user': user.user_id,
             'post_city': post_city,
             'post_street': post_street,
             'post_apartment_number': post_apartment_number,
