ca9060114dbce1501456536d53617c2a25f16b8e
Eran <Eranlevy9@gmail.com>
2023-12-31 13:06:00 +0200

    adding an AutoField in the db of UserInbox. user_id can't be, because there are multi messages for a user.

--- Inbox/models.py
+++ Inbox/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class UserInbox(models.Model):
-    user_id = models.CharField(max_length=50, primary_key=True,null=False,blank=False)
+    id = models.AutoField(primary_key=True)
+    user_id = models.CharField(max_length=50,null=False,blank=False)
     post_id = models.CharField(max_length=50, null=False, blank=False)
     user_message = models.CharField(max_length=200, default="")
     date = models.DateTimeField(auto_now_add=True)
