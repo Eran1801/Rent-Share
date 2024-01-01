a6a2f3af3eab25d83af15050d85e932bc6b771d7
Eran <Eranlevy9@gmail.com>
2024-01-01 12:00:56 +0200

    minor

--- Inbox/models.py
+++ Inbox/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.db.models import BigAutoField
 
 from Users.models import Users
 
 class UserInbox(models.Model):
-    message_id = models.ForeignKey(Users, on_delete=models.CASCADE)
-    user_id = models.CharField(max_length=50,null=False,blank=False)
+    message_id = BigAutoField(primary_key=True)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE,related_name = 'messages')
     post_id = models.CharField(max_length=50, null=False, blank=False)
     user_message = models.CharField(max_length=200, default="")
     date = models.DateTimeField(auto_now_add=True)
