989228fbb61f6625fca1e59ea5fefafc67235f94
Eran Levy <Eranlevy9@gmail.com>
2024-03-15 09:32:20 +0200

    minor changes inside utility in users.

--- Users/utilities.py
+++ Users/utilities.py
@@ -8,9 +8,6 @@ import re
 import hashlib
 import logging
 import os
-'''
-In this file there is all the helper function
-for the User app and maybe others'''
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -21,10 +18,12 @@ FROM_EMAIL = os.environ.get('COMPANY_EMAIL')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 def generate_random_digits() -> str:
+    '''Generate a random 4 digits number for forget password'''
     return ''.join(random.choice('0123456789') for _ in range(4))
 
-def send_email(sender_email,receiver_email,message,subject) -> None:
 
+def send_email(sender_email,receiver_email,message,subject) -> None:
+    '''Send an email to the user'''
     try:
         # Create a MIMEMultipart message
         msg = MIMEMultipart()
@@ -43,12 +42,13 @@ def send_email(sender_email,receiver_email,message,subject) -> None:
     except Exception as e:
         logger.error('Error send email: %s', e)
     
-
+    
 def check_valid_password(pas:str) -> bool:
+    '''check if the password is valid, it must contain at least one upper and one lower letter and number'''
 
     pattern = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)' # contains at least one upper and one lower letter and number.
 
-    return True if re.match(pattern,pas) and len(pas) >= 8 else False
+    return re.match(pattern,pas) and len(pas) >= 8
 
 
 def hash_password(plain_password:str) -> str:
@@ -62,14 +62,12 @@ def hash_password(plain_password:str) -> str:
 
 
 def email_exists(email:str) -> bool:
-    '''exists() method returns True if user_email already in the db'''
-
+    '''returns True if user_email already in the db'''
     return Users.objects.filter(user_email=email).exists()
 
 
 def phone_exists(phone:str)-> bool:
     '''returns True if at least one record matches the filter, and False if no records match.'''
-
     return Users.objects.filter(user_phone=phone).exists()
 
 
@@ -78,20 +76,17 @@ def full_name_check(full_name:str) -> bool:
     check if the full name is valid
     full name must be at least 4 characters and contain at least one space.
     '''
-
-    space_place = full_name.find(' ') # to ensure that there is a space in the full name
-    return True if len(full_name) >= 3 and space_place != -1 and len(full_name[space_place:]) > 0 else False
+    return len(full_name) >= 4 and full_name.count(' ') >= 1
 
 
 def phone_number_check(phone_number:str) -> bool:
     '''
     check if the phone number is valid.
-    phone number must be at least 10 characters.    
+    phone number must be at least 10 characters 
     '''
-
-    return True if len(phone_number) == 10 and phone_number.isdigit() else False
+    return len(phone_number) == 10
 
 
 def check_email_valid(email:str)-> bool:
     '''in registration, the email is checked by Django but when he change it we need to check it again'''
-    return True if email.count('@') == 1 and email.count('.') >= 1 else False
+    return email.count('@') == 1 and email.count('.') >= 1
