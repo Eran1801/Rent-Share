79b624afb4fb28d7bb06f832eda760d10c047b33
Eran Levy <Eranlevy9@gmail.com>
2024-03-21 20:31:29 +0200

    adding port var to connect to the db

--- .github/workflows/delete_old_verification_codes.py
+++ .github/workflows/delete_old_verification_codes.py
@@ -8,6 +8,7 @@ def delete_old_verification_codes():
     db_config = {
         'host': os.getenv('DB_HOST'),
         'user': os.getenv('DB_USER'),
+        'port': os.getenv('DB_PORT'),
         'passwd': os.getenv('DB_PASSWORD'),
         'database': os.getenv('DB_NAME')
     }
