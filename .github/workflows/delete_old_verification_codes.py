30357a9fa208723d1593b104e140eaeb3deebb8e
Eran Levy <Eranlevy9@gmail.com>
2024-03-21 14:26:37 +0200

    minor

--- .github/workflows/delete_old_verification_codes.py
+++ .github/workflows/delete_old_verification_codes.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from Users.models import PasswordResetCode
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TelAviv.settings")
 django.setup()
 
 
