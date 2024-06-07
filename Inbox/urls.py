75fc80139e076e1414e49209d1d3e2025cf053d3
Eran Levy <Eranlevy9@gmail.com>
2024-06-07 16:52:34 +0300

    minor

--- Inbox/urls.py
+++ Inbox/urls.py
@@ -4,6 +4,8 @@ from Inbox import views
 
 urlpatterns = [
     # Attach the right function to the right url
+
+    # move to Posts app maybe ? 
     re_path(r'^update_confirm_status/$', views.update_confirm_status_field, name='update_confirm_status'), 
     re_path(r'^get_all_user_messages/$', views.get_all_user_messages,name='get_all_user_messages'),
     re_path(r'^update_read_status/$',views.update_read_status,name='update_read_status'),
--- Users/urls.py
+++ Users/urls.py
@@ -5,15 +5,18 @@ from Users.auth import views_auth
 from Users.personal_info import views_personal_info
 
 urlpatterns = [
-    # Attach the right function to the right url
+
+    # authentication
     re_path(r'^register/$', views_auth.register, name='register'),
     re_path(r'^login/$', views_auth.login, name='login'),
     re_path(r'^delete_user/$', views_auth.delete_user, name='delete_user'),
 
+    # forgot-password flow
     re_path(r'^forget_password/$', views_forgot_pass.forget_password, name='forget_password'),
     re_path(r'verify_password_reset_code/$', views_forgot_pass.verify_code, name='verify_password_reset_code'),
     re_path(r'^reset_password/$', views_forgot_pass.reset_password, name='reset_password'),
 
+    # user profile
     re_path(r'^change_personal_info/$', views_personal_info.change_personal_info, name='change_personal_info'),
     re_path(r'^change_password/$', views_personal_info.change_password, name='change_password'),
     re_path(r'^change_profile_pic/$', views_personal_info.change_profile_picture, name='change_profile_pic'),
